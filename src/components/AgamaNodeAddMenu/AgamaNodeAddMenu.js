1a27ef6e0b9e27c1aa7ed8c1aaea84141895ee47
Thomas Gasmyr <Thomas@gluu.org>
2022-10-14 14:57:11 +0100

    added radio button for when node

--- src/components/AgamaNodeAddMenu/AgamaNodeAddMenu.js
+++ src/components/AgamaNodeAddMenu/AgamaNodeAddMenu.js
@@ -6,15 +6,11 @@ import MenuItem from '@mui/material/MenuItem'
 import AddCircleOutlined from '@mui/icons-material/AddCircleOutlined'
 import { useReactFlow } from 'reactflow'
 import { v4 as uuidv4 } from 'uuid'
-<<<<<<< HEAD
 import { Box } from '@mui/material'
 import Radio from '@mui/material/Radio'
 import RadioGroup from '@mui/material/RadioGroup'
 import FormControlLabel from '@mui/material/FormControlLabel'
 import FormLabel from '@mui/material/FormLabel'
-=======
-import Alert from '@mui/material/Alert'
->>>>>>> 688e5b9bea0ecd3e762881dd7e48916782b6da68
 
 export const AgamaNodeAddMenu = styled((props) => (
   <Menu
@@ -70,7 +66,7 @@ export default function CustomizedMenus({ data }) {
   }
 
   // When node condition
-  const [condition, setCondition] = useState('')
+  const [condition, setCondition] = useState('WhenTrue')
   const handleChange = (event) => {
     setCondition(event.target.value)
   }
@@ -84,25 +80,24 @@ export default function CustomizedMenus({ data }) {
   }
 
   // Node Menu generator
-  //const xPos = useRef(0)
+  const xPos = useRef(0)
   const flowInstance = useReactFlow()
 
   // Call Node
   const addCallNode = useCallback(() => {
     const newCallId = 'Call-' + uuidv4()
-    const xPos = data.position.x + 200
+    xPos.current += 400
     const newCallNode = {
       id: newCallId,
       position: {
-        x: xPos,
-        y: data.position.y
+        x: xPos.current,
+        y: 250,
       },
       type: 'call',
       data: {
         id: newCallId,
         type: 'Agama-call-Node',
         parentId: data.id,
-        position: { x: xPos, y: data.position.y },
       },
     }
     // edge
@@ -121,19 +116,18 @@ export default function CustomizedMenus({ data }) {
   // Trigger Node
   const addTriggerNode = useCallback(() => {
     const newTriggerNodeId = 'Trigger-' + uuidv4()
-    const xPos = data.position.x + 200
+    xPos.current += 400
     const newTriggerNode = {
       id: newTriggerNodeId,
       position: {
-        x: xPos,
-        y: data.position.y
+        x: xPos.current,
+        y: 250,
       },
       type: 'trigger',
       data: {
         id: newTriggerNodeId,
         type: 'Agama-trigger-Node',
         parentId: data.id,
-        position: { x: xPos, y: data.position.y },
       },
     }
 
@@ -153,19 +147,18 @@ export default function CustomizedMenus({ data }) {
   // RRF Node
   const addRrfNode = useCallback(() => {
     const newRrfId = 'RRF-' + uuidv4()
-    const xPos = data.position.x + 200
+    xPos.current += 400
     const newRrfNode = {
       id: newRrfId,
       position: {
-        x: xPos,
-        y: data.position.y
+        x: xPos.current,
+        y: 250,
       },
       type: 'rrf',
       data: {
         id: newRrfId,
         type: 'Agama-rrf-Node',
         parentId: data.id,
-        position: { x: xPos, y: data.position.y },
       },
     }
 
@@ -185,19 +178,18 @@ export default function CustomizedMenus({ data }) {
   // RFAC Node
   const addRfacNode = useCallback(() => {
     const newRfacNodeId = 'RFAC-' + uuidv4()
-    const xPos = data.position.x + 200
+    xPos.current += 400
     const newRfacNode = {
       id: newRfacNodeId,
       position: {
-        x: xPos,
-        y: data.position.y
+        x: xPos.current,
+        y: 250,
       },
       type: 'rfac',
       data: {
         id: newRfacNodeId,
         type: 'Agama-rfac-Node',
         parentId: data.id,
-        position: { x: xPos, y: data.position.y },
       },
     }
 
@@ -217,19 +209,18 @@ export default function CustomizedMenus({ data }) {
   // When Node
   const addWhenNode = useCallback(() => {
     const newWhenNodeId = 'When-' + uuidv4()
-    const xPos = data.position.x + 200
+    xPos.current += 400
     const newWhenNode = {
       id: newWhenNodeId,
       position: {
-        x: xPos,
-        y: data.position.y
+        x: xPos.current,
+        y: 250,
       },
       type: 'when',
       data: {
         id: newWhenNodeId,
         type: 'Agama-when-Node',
         parentId: data.id,
-        position: { x: xPos, y: data.position.y },
       },
     }
 
@@ -249,12 +240,12 @@ export default function CustomizedMenus({ data }) {
   // Repeat Node
   const addRepeatNode = useCallback(() => {
     const newRepeatNodeId = 'Repeat-' + uuidv4()
-    const xPos = data.position.x + 200
+    xPos.current += 400
     const newRepeatNode = {
       id: newRepeatNodeId,
       position: {
-        x: xPos,
-        y: data.position.y
+        x: xPos.current,
+        y: 250,
       },
       type: 'repeat',
       data: {
@@ -262,7 +253,6 @@ export default function CustomizedMenus({ data }) {
         type: 'Agama-repeat-Node',
         parentId: data.id,
         whenCondition: condition,
-        position: { x: xPos, y: data.position.y },
       },
     }
 
@@ -283,19 +273,18 @@ export default function CustomizedMenus({ data }) {
   // Log Node
   const addLogNode = useCallback(() => {
     const newLogNodeId = 'Log-' + uuidv4()
-    const xPos = data.position.x + 200
+    xPos.current += 400
     const newLogNode = {
       id: newLogNodeId,
       position: {
-        x: xPos,
-        y: data.position.y
+        x: xPos.current,
+        y: 250,
       },
       type: 'log',
       data: {
         id: newLogNodeId,
         type: 'Agama-log-Node',
         parentId: data.id,
-        position: { x: xPos, y: data.position.y },
       },
     }
 
@@ -315,19 +304,18 @@ export default function CustomizedMenus({ data }) {
   // Finish Node
   const addFinishNode = useCallback(() => {
     const newFinishNodeId = 'Finish-' + uuidv4()
-    const xPos = data.position.x + 200
+    xPos.current += 400
     const newFinishNode = {
       id: newFinishNodeId,
       position: {
-        x: xPos,
-        y: data.position.y
+        x: xPos.current,
+        y: 250,
       },
-      type: 'end',
+      type: 'finish',
       data: {
         id: newFinishNodeId,
         type: 'Agama-finish-Flow',
         parentId: data.id,
-        position: { x: xPos, y: data.position.y },
       },
     }
 
@@ -340,36 +328,25 @@ export default function CustomizedMenus({ data }) {
         target: newFinishNodeId,
       },
     ]
-    // data.type === 'Agama-start-Flow' ?
-    if (data.type === 'Agama-start-Flow') {
-      console.log('-------invalid connection-----')
-      return (
-        <Alert variant="outlined" severity="error">
-          Invalid connection — Can't connect Start to Finish node!
-        </Alert>
-      )
-    } else {
-      flowInstance.addNodes(newFinishNode)
-      flowInstance.setEdges((eds) => eds.concat(edges[0]))
-    }
+    flowInstance.addNodes(newFinishNode)
+    flowInstance.setEdges((eds) => eds.concat(edges[0]))
   }, [])
 
   // Quit Node
   const addQuitNode = useCallback(() => {
     const newQuitNodeId = 'Quit-' + uuidv4()
-    const xPos = data.position.x + 200
+    xPos.current += 400
     const newQuitNode = {
       id: newQuitNodeId,
       position: {
-        x: xPos,
-        y: data.position.y
+        x: xPos.current,
+        y: 250,
       },
       type: 'quit',
       data: {
         id: newQuitNodeId,
         type: 'Agama-quit-Node',
         parentId: data.id,
-        position: { x: xPos, y: data.position.y },
       },
     }
 
@@ -385,6 +362,14 @@ export default function CustomizedMenus({ data }) {
     flowInstance.addNodes(newQuitNode)
     flowInstance.setEdges((eds) => eds.concat(edges[0]))
   }, [])
+
+  const canShowFinishItem = (data) => {
+    if (data.type === 'Agama-finish-Flow' || data.type === 'Agama-start-Flow') {
+      return false
+    } else {
+      return true
+    }
+  }
 
   return (
     <>
@@ -453,9 +438,11 @@ export default function CustomizedMenus({ data }) {
         <MenuItem onClick={addRepeatNode} disableRipple>
           Repeat
         </MenuItem>
-        <MenuItem onClick={addFinishNode} disableRipple>
-          Finish
-        </MenuItem>
+        {canShowFinishItem(data) && (
+          <MenuItem onClick={addFinishNode} disableRipple>
+            Finish
+          </MenuItem>
+        )}
       </AgamaNodeAddMenu>
     </>
   )
